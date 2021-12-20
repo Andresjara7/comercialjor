a52f2e64cfde45b4fa4b633514bea36d2be69c2c
Andres Jaramillo <96316014+Andresjara7@users.noreply.github.com>
2021-12-20 16:31:27 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let ubicacionPrincipal=window.pageYOffset;
+
+AOS.init();
+window.addEventListener("scroll", function(){
+let desplazamientoActual=window.pageYOffset;
+if(ubicacionPrincipal>=desplazamientoActual){
+    this.document.getElementsByTagName("nav")[0].style.top="0px"
+}else{
+    this.document.getElementsByTagName("nav")[0].style.top="-100px"
+}
+ubicacionPrincipal=desplazamientoActual;
+})
+
+//menu
+
+let enlacesHeader=document.querySelectorAll(".enlaces-header")[0];
+let semaforo=true;
+
+document.querySelectorAll(".hamburguer")[0].addEventListener("click",function(){
+    if(semaforo){
+        document.querySelectorAll(".hamburguer")[0].style.color = "#fff";
+        semaforo=false;
+    }else{
+        document.querySelectorAll(".hamburguer")[0].style.color = "#000";
+        semaforo=true;
+    }
+
+    enlacesHeader.classList.toggle("menudos")
+})
